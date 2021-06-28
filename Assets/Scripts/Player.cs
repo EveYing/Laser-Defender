95387434213aec226e37297055c99f205763e4fd
Eve Ying <eveying96@gmail.com>
2021-06-28 16:03:49 -0400

    Make Player Shoot

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,29 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
 
     float xMin;
     float xMax;
     float yMin;
     float yMax;
-    float padding = 0.5f;
+    float paddingX;
+    float paddingY;
+    float coolDownTime = 0.2f;
+    float shootingTime = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         Camera gameCamera = Camera.main;
+        Renderer renderer = GetComponent<Renderer>();
 
-        xMin = gameCamera.ViewportToWorldPoint(Vector3.zero).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(Vector3.one).x - padding;
+        paddingX = renderer.bounds.size.x / 2f;
+        paddingY = renderer.bounds.size.y / 2f;
 
-        yMin = gameCamera.ViewportToWorldPoint(Vector3.zero).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(Vector3.one).y - padding;
+        xMin = gameCamera.ViewportToWorldPoint(Vector3.zero).x + paddingX;
+        xMax = gameCamera.ViewportToWorldPoint(Vector3.one).x - paddingX;
+
+        yMin = gameCamera.ViewportToWorldPoint(Vector3.zero).y + paddingY;
+        yMax = gameCamera.ViewportToWorldPoint(Vector3.one).y - paddingY;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
     }
 
     private void Move()
@@ -39,5 +49,22 @@ public class Player : MonoBehaviour
         var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            shootingTime += Time.deltaTime;
+            if (shootingTime >= coolDownTime)
+            {
+                shootingTime = 0f;
+                Vector3 firePosition = new Vector3(transform.position.x,
+                                   transform.position.y + paddingY,
+                                   transform.position.z);
+                GameObject laser = Instantiate(laserPrefab, firePosition, Quaternion.identity);
+                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            }
+        }
     }
 }
